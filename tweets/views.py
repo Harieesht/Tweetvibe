7049fc1f15339efb155e77d8ac921e0ffd81ff0c
Harieesh Rakhavandaran <harirakha12@gmail.com>
2024-04-09 11:55:36 +0530

    Tweetcreationform

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse,JsonResponse,Http404
 from .models import *
+from .forms import TweetForm
+from django.conf import settings
+
+ALLOWED_HOSTS=settings.ALLOWED_HOSTS
 
 #homepage view
 def home_page(request,*args,**kwargs):
@@ -12,10 +16,10 @@ def home_page(request,*args,**kwargs):
 #tweetlistview(Returns tweets in json data)
 def tweet_list_view(request,*args,**kwargs):
     queryset=Tweet.objects.all()
-    tweets_list=[{"id":tweet.id,"content":tweet.content} for tweet in queryset]
+    tweets_list=[{"id":tweet.id,"content":tweet.content,"likes":12} for tweet in queryset]
     data={
         "isUser":False,
-        "response":tweets_list
+        "response":tweets_list,
     }
     return JsonResponse(data)
 
@@ -37,3 +41,17 @@ def tweet_detail_view(request,tweet_id,*args,**kwargs):
         status=404
     return JsonResponse(data,status=status)
 
+#tweet creation view
+#rendering form in homepage instead seperate "form.html" page
+#next_url indicates current homepage url
+def tweet_create_view(request,*args,**kwargs):
+    form=TweetForm(request.POST or None)
+    next_url=request.POST.get('next') or None
+    print(next_url)
+    if form.is_valid():
+        obj=form.save(commit=False)
+        obj.save()
+        if next_url != None:
+            return redirect(next_url)
+        form=TweetForm()
+    return render(request,'components/form.html',context={"form":form})
--- tweetvibe/settings.py
+++ tweetvibe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@bcqx@tc_i&xzspcahl4wotu2_gpmyyo4*v#qdwg@!u*-eg&f+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
