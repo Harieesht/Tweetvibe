6238936b35b5aa6d8b3c9072d589001c985c7c3f
Harieesh Rakhavandaran <harirakha12@gmail.com>
2024-04-10 22:35:47 +0530

    tweet submit

--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,JsonResponse,Http404
 from .models import *
 from .forms import TweetForm
 from django.conf import settings
+ 
 
 ALLOWED_HOSTS=settings.ALLOWED_HOSTS
 
@@ -16,7 +17,7 @@ def home_page(request,*args,**kwargs):
 #tweetlistview(Returns tweets in json data)
 def tweet_list_view(request,*args,**kwargs):
     queryset=Tweet.objects.all()
-    tweets_list=[{"id":tweet.id,"content":tweet.content,"likes":12} for tweet in queryset]
+    tweets_list=[tweet.serialize() for tweet in queryset]
     data={
         "isUser":False,
         "response":tweets_list,
@@ -41,16 +42,22 @@ def tweet_detail_view(request,tweet_id,*args,**kwargs):
         status=404
     return JsonResponse(data,status=status)
 
+#usermade is_ajax function because it didnt work on my machine
+def is_ajax(request):
+  return request.headers['x-requested-with'] == 'XMLHttpRequest'
+
 #tweet creation view
 #rendering form in homepage instead seperate "form.html" page
 #next_url indicates current homepage url
 def tweet_create_view(request,*args,**kwargs):
+    print("ajax",is_ajax(request))
     form=TweetForm(request.POST or None)
     next_url=request.POST.get('next') or None
-    print(next_url)
     if form.is_valid():
         obj=form.save(commit=False)
         obj.save()
+        if is_ajax(request):
+            return JsonResponse(obj.serialize(),status=201)
         if next_url != None:
             return redirect(next_url)
         form=TweetForm()
